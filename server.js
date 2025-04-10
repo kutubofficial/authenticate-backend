4b5b6cb287e63ccc886ca9dafc9ada6965bf4bd7
Kutubuddin Ansari <kutubofficial599@gmail.com>
2025-04-10 18:16:56 +0530

    Add root route to test Render deployment

--- server.js
+++ server.js
@@ -14,6 +14,5 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
     res.send("Backend is live and running!");
   });
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
